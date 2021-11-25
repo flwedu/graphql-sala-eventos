8aa702bd7aa9227a40c37ac9995abb3ae9b99fc6
Eduardo Aquino <edutraquino@gmail.com>
2021-11-25 19:16:12 -0300

    index creates the instances of PrismaClient

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-const { GraphQLServer } = require("graphql-yoga");
-const path = require("path");
-const resolvers = require("./resolvers/resolvers");
+import { PrismaClient } from "@prisma/client";
+import { GraphQLServer } from "graphql-yoga";
+import path from "path";
+import usersResolvers from "./resolvers/usersResolvers";
+
+export const prisma = new PrismaClient();
 
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, "schemas/schema.graphql"),
-  resolvers,
+  resolvers: [usersResolvers],
 });
 
 server.start(() => console.log("server is running on localhost:4000"));
