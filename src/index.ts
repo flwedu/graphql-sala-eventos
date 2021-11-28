4ed535e9b5afa6dd2c87009fbbe05779e9cf4f26
Eduardo Aquino <edutraquino@gmail.com>
2021-11-28 16:41:47 -0300

    link new resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 import { GraphQLServer } from "graphql-yoga";
 import path from "path";
+import roomEventPresenceResolver from "./resolvers/roomEventPresenceResolver";
+import roomEventsResolver from "./resolvers/roomEventsResolver";
+import roomResolver from "./resolvers/roomResolver";
 import usersResolver from "./resolvers/usersResolver";
 
 export const prisma = new PrismaClient();
 
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, "schemas/schema.graphql"),
-  resolvers: [usersResolver],
+  resolvers: [usersResolver, roomResolver, roomEventsResolver, roomEventPresenceResolver],
 });
 
 server.start(() => console.log("server is running on localhost:4000"));
