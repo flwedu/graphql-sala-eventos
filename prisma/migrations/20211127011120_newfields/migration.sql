9b4512b0a1f4f3cdbbc19e11ae3d626bc01ab4bb
Eduardo <edutraquino@gmail.com>
2021-12-07 20:36:04 -0300

    excluding postgre migrations

--- prisma/migrations/20211127011120_newfields/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Acess" AS ENUM ('PUBLIC', 'ONLYINVITED', 'ONLYACCEPTED');
-
--- AlterTable
-ALTER TABLE "RoomEvent" ADD COLUMN     "acess" "Acess" NOT NULL DEFAULT E'PUBLIC';
